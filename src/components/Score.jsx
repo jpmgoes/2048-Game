30a32fc688e5b9e1e17faa5660b389d63a02b21a
JPMouraG <jp.mouragoes@gmail.com>
2020-11-14 23:37:41 -0300

    score context added

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -7,7 +7,6 @@ export function Score() {
   let bestScore = localStorage.getItem("bestScore");
   
   if (bestScore < score) {
-    
     bestScore = score;
     localStorage.setItem("bestScore", bestScore);
   }
